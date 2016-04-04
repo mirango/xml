4ed3e821986e01f0c7d7cf0f126c7586583b2623
wlMalk <wm.wexem@gmail.com>
2016-04-04 06:54:22 -0500

    Implemented XML renderer

---
+++ renderer.go
@@ -0,0 +1,34 @@
+package json
+
+import (
+	"encoding/xml"
+	"io"
+
+	"github.com/wlMalk/mirango/framework"
+)
+
+type Renderer struct{}
+
+func New() *Renderer {
+	return &Renderer{}
+}
+
+func (r *Renderer) MimeType() string {
+	return framework.MIME_XML
+}
+
+func (r *Renderer) RenderToWriter(wr io.Writer, c framework.Context, data interface{}) error {
+	b, err := r.Render(c, data)
+	if err != nil {
+		return err
+	}
+	_, err = wr.Write(b)
+	return err
+}
+
+func (r *Renderer) Render(c framework.Context, data interface{}) ([]byte, error) {
+	if c.IsIndented() {
+		return xml.MarshalIndent(data, "", " ")
+	}
+	return xml.Marshal(data)
+}
